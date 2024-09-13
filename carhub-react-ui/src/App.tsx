5fc590fc2da9567e7700d29967a270b4975a3981
BoyanK95 <boyan_k_95@abv.bg>
2024-09-13 17:22:20 +0300

    style: checkbox unchecked color to white

--- carhub-react-ui/src/App.tsx
+++ carhub-react-ui/src/App.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Navbar from "./components/navigation/Navbar";
 import Home from "./components/screens/Home";
